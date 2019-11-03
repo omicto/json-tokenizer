5aad02d1e9fc24f3240f2ebf92a01e6981f8e4d7
Sebastian <tevas.dlr@gmail.com>
2019-11-03 12:06:36 -0700

    Add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple JSON "Tokenizer"
+Shows the tokens that an input JSON text is made of.
+
+### Usage
+```
+java com.cjs.lexer.Main < input
+```
+Where input is a text file containing JSON formatted text.
--- src/com/cjs/lexer/Main.java
+++ src/com/cjs/lexer/Main.java
@@ -1,6 +1,19 @@
 package com.cjs.lexer;
 
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        while(scan.hasNextLine()){
+            sb.append(scan.nextLine());
+        }
+        Lexer lex = new Lexer(sb.toString());
+        List<Token> tokens = lex.tokenize();
+        tokens.forEach(System.out::println);
+    }
 
 }
